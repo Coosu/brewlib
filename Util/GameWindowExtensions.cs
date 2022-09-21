c1b05532f0b7e5b1b4138cca6352f499d6fe1236
MikiraSora <mikirasora0409@126.com>
2022-09-21 15:37:25 +0800

    fix GameWindow.GetWindowHandle() break change by OpenTK4.x (not test)

--- Util/GameWindowExtensions.cs
+++ Util/GameWindowExtensions.cs
@@ -6,13 +6,13 @@ namespace BrewLib.Util
 {
     public static class GameWindowExtensions
     {
-        public static IntPtr GetWindowHandle(this GameWindow window)
+        public static unsafe IntPtr GetWindowHandle(this GameWindow window)
         {
             var handle = Native.FindProcessWindow(window.Title);
             if (handle != IntPtr.Zero) return handle;
 
             // This handle is incorrect for some users, only use it if the window couldn't be found by title
-            return window.WindowInfo.Handle;
+            return new IntPtr(window.WindowPtr);
         }
     }
 }
